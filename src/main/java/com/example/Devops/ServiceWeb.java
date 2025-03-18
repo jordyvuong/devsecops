c318c6d0fd61bc2fec79dc4fd1155950f51d958c
jordyvuong <joordyr2@gmail.com>
2025-03-18 16:52:33 +0100

    Adding /cars

--- src/main/java/com/example/Devops/ServiceWeb.java
+++ src/main/java/com/example/Devops/ServiceWeb.java
@@ -1,12 +1,13 @@
 package com.example.Devops;
-
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
 
 @RestController
 public class ServiceWeb {
-    @GetMapping("/")
-    public String hello() {
-        return "hello";
+    @GetMapping("/cars")
+    public Car cars(){
+        Car car = new Car("AA11BB", "Maserati", 2000);
+        return car;
     }
 }
